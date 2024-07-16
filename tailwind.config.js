78830bf8a1ba11d6988818f1f107d86ab88097ed
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-07-15 19:26:45 -0600

    Agregar config css, ademas el general SASS

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,31 @@ export default {
     "./index.html",
     "./src/**/*.{js,jsx}",
   ],
+  darkMode: 'class', // or 'media'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#FF724C', // Naranja
+          dark: '#F4F4F8', // Gris claro (for dark mode)
+        },
+        secondary: {
+          DEFAULT: '#FDBF50', // Amarillo
+          dark: '#2A2C41', // Gris oscuro (for dark mode)
+        },
+        background: {
+          DEFAULT: '#F4F4F8', // Gris claro
+          dark: '#FF724C', // Naranja (for dark mode)
+        },
+        text: {
+          DEFAULT: '#2A2C41', // Gris oscuro
+          dark: '#FDBF50', // Amarillo (for dark mode)
+        },
+      },
+      textColor: {
+        grayish: '#666',
+      },
+    },
   },
   plugins: [
     require('flowbite/plugin')
