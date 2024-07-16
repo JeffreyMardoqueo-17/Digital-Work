1540d478f6ea12aa4c34ebb636f5f2ff47a35fa1
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-07-15 19:15:18 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import NavBar from './components/NavBar'
+function App() {
+
+  return (
+    <>
+      <div className="">
+        {/* <NavBar /> */}
+        <h1>AQUI TODO BIEN</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
