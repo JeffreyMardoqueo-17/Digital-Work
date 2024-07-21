1c5ec68e3905c4c1e89e0b0a25586aea19efe799
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-07-20 21:49:33 -0600

    DISEÑO DE LA PARTE DEL SERVICE

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -15,7 +15,7 @@ const ImageCarousel = ({ images }) => {
     );
 };
 
-const Card = ({ title, description, listItems, images }) => {
+const Card = ({ title, description, images }) => {
     return (
         <div className="block  rounded-lg bg-white/10 backdrop-blur-md shadow-md dark:bg-gray-800/70 dark:text-white">
             <div className="relative overflow-hidden bg-cover bg-no-repeat">
@@ -25,13 +25,7 @@ const Card = ({ title, description, listItems, images }) => {
                 <h5 className="mb-2 text-xl font-medium leading-tight">{title}</h5>
                 <p className="text-base">{description}</p>
             </div>
-            <ul className="w-full">
-                {listItems.map((item, index) => (
-                    <li key={index} className="w-full border-b-2 border-neutral-100 border-opacity-100 px-6 py-3 dark:border-gray-700">
-                        {item}
-                    </li>
-                ))}
-            </ul>
+            
             <div className="p-6">
                 <button type="button" class="focus:outline-none text-white bg-lightTheme-primary hover:bg-blue-600 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Descargar Catalogo</button>
             </div>
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,4 +1,4 @@
-
+// Services.js
 import React from 'react';
 import Card from '../components/Card';
 
@@ -21,32 +21,26 @@ const Services = () => {
         "https://tecdn.b-cdn.net/img/new/standard/city/070.jpg"
     ];
 
-    const listItems = [
-        "Cras justo odio",
-        "Dapibus ac facilisis in",
-        "Vestibulum at eros"
-    ];
-
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-8 container mx-auto ">
-            <Card
-                title="Productos de Papeleria"
-                description="Some quick example text to build on the card title and make up the bulk of the card's content."
-                listItems={listItems}
-                images={images1}
-            />
-            <Card
-                title="Estampados"
-                description="Some quick example text to build on the card title and make up the bulk of the card's content."
-                listItems={listItems}
-                images={images2}
-            />
-            <Card
-                title="Productos Digitales"
-                description="Some quick example text to build on the card title and make up the bulk of the card's content."
-                listItems={listItems}
-                images={images3}
-            />
+        <div className="bg-dots-pattern bg-cover p-4 container mx-auto">
+            <h1 className="text-3xl font-bold text-center mb-8 dark:text-white">TIPOS DE SERVICIOS</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-8 container mx-auto">
+                <Card
+                    title="Productos de Papelería"
+                    description="Ofrecemos una amplia gama de productos de papelería, incluyendo cuadernos, pegatinas, colores, páginas, cartulinas, silicona y más. Perfecto para tus necesidades estudiantiles y de oficina."
+                    images={images1}
+                />
+                <Card
+                    title="Estampados"
+                    description="Diseñamos y estampamos tazas, termos, llaveros, camisas, gorras y otros artículos personalizados a medida del cliente. Ideal para promociones y eventos especiales."
+                    images={images2}
+                />
+                <Card
+                    title="Productos Digitales"
+                    description="Encuentra una variedad de productos digitales como audífonos, cargadores, mouses, teclados y otros accesorios tecnológicos. Todo lo que necesitas para tu configuración tecnológica."
+                    images={images3}
+                />
+            </div>
         </div>
     );
 }
