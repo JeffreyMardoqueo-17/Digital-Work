7702be59715864c6de0f35840ecf5547b11183cf
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-07-23 11:33:19 -0600

    Aqui esta casi listo pero aun falta el diseño de la seccion de pagos

--- src/sections/Pagos.jsx
+++ src/sections/Pagos.jsx
@@ -2,36 +2,35 @@ import React from 'react';
 
 const Pagos = () => {
     return (
-        <div className='gradiente container mx-auto'>
-            <div className='mx-auto p-6 backdrop-filter backdrop-blur-xl bg-white bg-opacity-30 container h-3/4'>
-                <div className="flex flex-col md:flex-row p-6">
+        <div className='gradiente container mx-auto p-1'>
+            <div className='mx-auto p-4 backdrop-filter backdrop-blur-xl bg-white dark:bg-darkTheme-background dark:bg-opacity-30 bg-opacity-30 h-3/4'>
+                <div className="flex flex-col md:flex-row p-4">
                     {/* Lado izquierdo */}
-                    <div className="flex justify-center items-center md:w-1/2 h-auto p-16">
+                    <div className="flex justify-center items-center md:w-1/2 h-auto p-4">
                         <img src="../svg/pagoMujer1.svg" alt="Pago Mujer" className="w-auto h-auto object-cover rounded-lg" />
                     </div>
                     {/* Lado derecho */}
-                    <div className="md:w-1/2 p-6 flex flex-col justify-center">
-                        <h2 className="text-6xl font-bold mb-4 text-lightTheme-primary">Realiza tus pagos de forma rápida y sencilla</h2>
-                        <p className="text-gray-700 mb-2">Paga tus facturas de:</p>
-                        <ul className="list-disc list-inside text-gray-700">
+                    <div className="md:w-1/2 p-4 flex flex-col justify-center">
+                        <h2 className="text-3xl sm:text-4xl md:text-6xl font-bold mb-4 text-lightTheme-primary dark:text-white">Realiza tus pagos de forma rápida y sencilla</h2>
+                        <p className="text-gray-700 dark:text-gray-300 mb-2">Paga tus facturas de:</p>
+                        <ul className="list-disc list-inside text-gray-700 dark:text-gray-300 mb-4">
                             <li>Cable</li>
                             <li>Internet</li>
                             <li>Energía eléctrica</li>
                             <li>Agua potable</li>
                             <li>Teléfono</li>
                         </ul>
-                        <p className="text-gray-700 mt-4">Todo en un solo lugar, de manera segura y confiable.</p>
+                        <div className='flex flex-wrap justify-center md:justify-around items-center p-1'>
+                            <img src="../img/AESCLESA.png" alt="Empresa 1" className="w-12 h-auto object-contain m-2 md:w-20" />
+                            <img src="../img/claroLogo.png" alt="Empresa 2" className="w-12 h-auto object-contain m-2 md:w-20" />
+                            <img src="../img/claroLogo.png" alt="Empresa 2" className="w-12 h-auto object-contain m-2 md:w-20" />
+                            <img src="../img/AESCLESA.png" alt="Empresa 4" className="w-12 h-auto object-contain m-2 md:w-20" />
+                            <img src="../img/AESCLESA.png" alt="Empresa 5" className="w-12 h-auto object-contain m-2 md:w-20" />
+                            <img src="../img/AESCLESA.png" alt="Empresa 6" className="w-12 h-auto object-contain m-2 md:w-20" />
+                            <img src="../img/AESCLESA.png" alt="Empresa 7" className="w-12 h-auto object-contain m-2 md:w-20" />
+                        </div>
+                        <p className="text-gray-700 dark:text-gray-300 mt-4">Todo en un solo lugar, de manera segura y confiable.</p>
                     </div>
-                </div>
-                {/* Lado inferior con los íconos de las empresas */}
-                <div className='flex flex-wrap justify-center md:justify-around items-center p-4'>
-                    <img src="../img/AESCLESA.png" alt="Empresa 1" className="w-20 h-auto object-contain m-2" />
-                    <img src="../img/claroLogo.png" alt="Empresa 2" className="w-20 h-auto object-contain m-2" />
-                    <img src="../img/claroLogo.png" alt="Empresa 2" className="w-20 h-auto object-contain m-2" />
-                    <img src="../img/AESCLESA.png" alt="Empresa 4" className="w-20 h-auto object-contain m-2" />
-                    <img src="../img/AESCLESA.png" alt="Empresa 5" className="w-20 h-auto object-contain m-2" />
-                    <img src="../img/AESCLESA.png" alt="Empresa 6" className="w-20 h-auto object-contain m-2" />
-                    <img src="../img/AESCLESA.png" alt="Empresa 7" className="w-20 h-auto object-contain m-2" />
                 </div>
             </div>
         </div>
